473d57a7372694d355a42679ca32cadc0b19dea7
Sergey Chazov <oitv18@gmail.com>
2018-12-12 12:45:34 +0300

    решил задачи до 8 со страницы http://code.mu/tasks/javascript/jquery/osnovy-raboty-s-jquery.html

--- myScript.js
+++ myScript.js
@@ -13,4 +13,12 @@ $('#myButton2').click(function() {
 	$("#myLi2").hide(900);/* Act on the event */
 });	
 
+$('h3').html('!!!').css({color: 'green', fontWeight: 'bold'});
+$('.zzz').html('!!!').css('color', 'red');
+/*$('.zzz').html('!!!');
+$(':headers.zzz').css('color', 'red');*/
+$('li').each(
+	function(index, elem){
+		$(this).html(index);
+	});
 });
