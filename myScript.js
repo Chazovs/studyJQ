770ab055e625cab3d8be87531816387edab78a31
Sergey Chazov <oitv18@gmail.com>
2018-12-12 11:59:04 +0300

    jq заработало. есть два разных способа скрывать элементы

--- myScript.js
+++ myScript.js
@@ -1,2 +1,16 @@
 var x=5;
 console.log(x);
+ $(document).ready(function(){
+console.log('Ваша версия jQuery ' + jQuery.fn.jquery);
+
+$('#hidebutton').click(function() {
+	
+	$('#leftBlock').fadeToggle(200);/* Act on the event */
+});
+
+$('#myButton2').click(function() {
+	
+	$("#myLi2").hide(900);/* Act on the event */
+});	
+
+});
